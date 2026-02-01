93e4de0ac93e551d16c543b6abf105247b6b322f
Yash Wankhade <yashwankhade5@gmail.com>
2026-02-01 09:55:02 -0800

    first commit

--- programs/anchor-escrow-q4-25/src/instructions/take.rs
+++ programs/anchor-escrow-q4-25/src/instructions/take.rs
@@ -12,12 +12,64 @@ use anchor_spl::{
 
 use crate::Escrow;
 
-// #[derive(Accounts)]
-// pub struct Take<'info> {
-//      TODO: Implement Take Accounts
-// }
+#[derive(Accounts)]
+pub struct Take<'info> {
+    #[account(mut)]
+    pub taker: Signer<'info>,
+    pub maker: SystemAccount<'info>,
+    #[account(
+        mint::token_program = token_program,
+        constraint=mint_a.key() == escrow.mint_a
+    )]
+    pub mint_a: InterfaceAccount<'info, Mint>,
+    #[account(
+        mint::token_program = token_program
+    )]
+    pub mint_b: InterfaceAccount<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer=taker,
+        associated_token::mint = mint_a,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program
+    )]
+    pub taker_ata_a: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_b,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program
+    )]
+    pub taker_ata_b: InterfaceAccount<'info,TokenAccount>,
+     #[account(
+        init_if_needed,
+        payer=taker,
+        associated_token::mint = mint_b,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program
+    )]
+    pub maker_ata_b: Box<InterfaceAccount<'info, TokenAccount>>,
+       #[account(
+        mut,
+        close = taker,
+        seeds = [b"escrow", maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
+        bump = escrow.bump
+    )]
+    pub escrow: Account<'info, Escrow>,
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority = escrow,
+        associated_token::token_program = token_program
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
 
-// impl<'info> Take<'info> {
-//      TODO: Implement Take Instruction
-//      Includes Deposit, Withdraw and Close Vault
-// }
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Take<'info> {
+    //  TODO: Implement Take Instruction
+    //  Includes Deposit, Withdraw and Close Vault
+}
